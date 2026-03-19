9f7567f622957a13ba720aeee5a1d8387eb68b3a
sepp9835 <sa5473@gymnasium-sorghvliet.nl>
2026-03-19 10:57:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 
   $("#go_undercover").on('click', function(){
-    website = prompt("Enter a website\n('https://youtube.com', etc)");
+    website = prompt("typ een website of paste hem\n('https://youtube.com', etc)");
     window.open(website, "", "width=1000,height=1000");
   });
 
